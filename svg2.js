2ca3fd0794a47b979e142ddfd9ae6536a3c8c4ea
Nydroam <rayw.u17@gmail.com>
2016-04-21 14:56:26 -0400

    got something working?

--- svg2.js
+++ svg2.js
@@ -4,13 +4,16 @@ var b2 = document.getElementById("remove");
 var ballList = [];
 var colors = ["honeydew", "skyblue", "plum", "tan", "cornsilk", "brown", "dodgerblue"];
 
+var dist = function(a,b,c,d) {
+    return Math.sqrt((a-b)*(a-b) + (c-d)*(c-d));
+}
 var Ball = function(){
     var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     var r = Math.floor((Math.random() * 35)  + 15);
     var x = Math.floor((Math.random() * (500-2*r)) + r + 3);
     var y = Math.floor((Math.random() * (500-2*r)) + r + 3);
-    var dx = Math.random()*3;
-    var dy = Math.random()*3;
+    var dx = Math.random()*2+1;
+    var dy = Math.random()*2+1;
     c.setAttribute( "cx", x );
     c.setAttribute( "cy", y );
     c.setAttribute( "r", r );
@@ -28,8 +31,11 @@ var Ball = function(){
 	c.setAttribute( "cx", x );
 	c.setAttribute( "cy", y );
     }
-    var dist = function(a,b,c,d) {
-	return Math.sqrt((a-b)*(a-b) + (c-d)*(c-d));
+    var getx = function(){
+        return x;
+    }
+    var gety = function(){
+        return y;
     }
     var flip = function() {
 	dx*=-1;
@@ -40,12 +46,13 @@ var Ball = function(){
 	for(i = 0; i < ballList.length; i++){
 	    var other = ballList[i];
 	    if(c!=other.c) {
-		if (dist(x, other.x, y, other.y) < r + other.r) {
-		    console.log(dist(x, other.x, y, other.y));
-		    console.log(r + other.r);
-		    //flip();
-		    //other.flip();
-		    //inc();
+		if (dist(x, other.x(), y, other.y()) < r + other.r) {
+            console.log("colliding");
+		    //console.log(x + " " + other.x + " " + y + " " + other.y);
+		    //console.log(r + other.r);
+		    flip();
+		    other.flip();
+            inc();
 		}
 	    }
 	}
@@ -54,12 +61,13 @@ var Ball = function(){
     	inc: inc,
 	flip: flip,
 	collide: collide,
-	x: x,
-	y: y,
+	x: getx,
+	y: gety,
 	r: r,
 	c: c
     }
 }
+
 
 var addBall = function(){
     var b = Ball();
@@ -71,8 +79,8 @@ var addBall = function(){
 var removeBall = function(){
   var c = document.getElementsByTagName("circle");
   if(c[0]){
+      ballList.splice(0,1);
       c[0].remove();
-      ballList[0].remove();
   }
 }
 
